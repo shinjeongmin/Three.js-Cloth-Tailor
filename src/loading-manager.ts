0ee4b432c369a959871eb3cd5202db45829908fe
shinjeongmin <sin37minduke@naver.com>
2024-05-03 05:37:27 +0900

    feat: modify loading manager function return.

--- src/loading-manager.ts
+++ src/loading-manager.ts
@@ -1,20 +1,21 @@
 import { LoadingManager } from 'three'
-let loadingManager: LoadingManager
 
-  {
-    loadingManager = new LoadingManager()
+export function setLoadingManager(): LoadingManager{
+  const loadingManager = new LoadingManager()
 
-    loadingManager.onStart = () => {
-      console.log('loading started')
-    }
-    loadingManager.onProgress = (url, loaded, total) => {
-      console.log('loading in progress:')
-      console.log(`${url} -> ${loaded} / ${total}`)
-    }
-    loadingManager.onLoad = () => {
-      console.log('loaded!')
-    }
-    loadingManager.onError = () => {
-      console.log('❌ error while loading')
-    }
+  loadingManager.onStart = () => {
+    console.log('loading started')
   }
+  loadingManager.onProgress = (url, loaded, total) => {
+    console.log('loading in progress:')
+    console.log(`${url} -> ${loaded} / ${total}`)
+  }
+  loadingManager.onLoad = () => {
+    console.log('loaded!')
+  }
+  loadingManager.onError = () => {
+    console.log('❌ error while loading')
+  }
+
+  return loadingManager
+}
